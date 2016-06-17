f94fdaa68ec5b64210a2c21bd1ac78b7bc28880c
escheuer <erscheuerlein@gmail.com>
2016-06-17 04:07:37 +0000

    Uploaded Project
    I uploaded the project from my hard drive to github.

---
+++ nextFlag.js
@@ -0,0 +1,680 @@
+function nextFlag() {
+  var country = "";
+  var countryName = "";
+  var random = Math.round(Math.random()*223);
+  switch(random) {
+    case 1:
+      country = "AFGH"; countryName = "Afghanistan";
+      break;
+    case 2:
+      country = "OAUN"; countryName = "African Union";
+      break;
+    case 3:
+      country = "ALBA"; countryName = "Albania";
+      break;
+    case 4:
+      country = "ALGE"; countryName = "Algeria";
+      break;
+    case 5:
+      country = "ANDR"; countryName = "Andorra";
+      break;
+    case 6:
+      country = "AGLA"; countryName = "Angola";
+      break;
+    case 7:
+      country = "ANTA"; countryName = "Antarctica";
+      break;
+    case 8:
+      country = "ANBA"; countryName = "Antigua & Barbuda";
+      break;
+    case 9:
+      country = "ARGE"; countryName = "Argentina";
+      break;
+    case 10:
+      country = "ARME"; countryName = "Armenia";
+      break;
+    case 11:
+      country = "ARUB"; countryName = "Aruba";
+      break;
+    case 12:
+      country = "ASTL"; countryName = "Australia";
+      break;
+    case 13:
+      country = "AUST"; countryName = "Austria";
+      break;
+    case 14:
+      country = "AZER"; countryName = "Azerbaijan";
+      break;
+    case 15:
+      country = "BHMS"; countryName = "The Bahamas";
+      break;
+    case 16:
+      country = "BHRN"; countryName = "Bahrain";
+      break;
+    case 17:
+      country = "BNGL"; countryName = "Bangladesh";
+      break;
+    case 18:
+      country = "BRBD"; countryName = "Barbados";
+      break;
+    case 19:
+      country = "BLRU"; countryName = "Belarus";
+      break;
+    case 20:
+      country = "BELG"; countryName = "Belgium";
+      break;
+    case 21:
+      country = "BLZE"; countryName = "Belize";
+      break;
+    case 22:
+      country = "BNIN"; countryName = "Benin";
+      break;
+    case 23:
+      country = "BERM"; countryName = "Bermuda";
+      break;
+    case 24:
+      country = "BHUT"; countryName = "Bhutan";
+      break;
+    case 25:
+      country = "BLVA"; countryName = "Bolivia";
+      break;
+    case 26:
+      country = "BOHE"; countryName = "Bosnia";
+      break;
+    case 27:
+      country = "BOTS"; countryName = "Botswana";
+      break;
+    case 28:
+      country = "BRAZ"; countryName = "Brazil";
+      break;
+    case 29:
+      country = "BRUN"; countryName = "Brunei";
+      break;
+    case 30:
+      country = "BULG"; countryName = "Bulgaria";
+      break;
+    case 31:
+      country = "BUFA"; countryName = "Burkina Faso";
+      break;
+    case 32:
+      country = "BURU"; countryName = "Burundi";
+      break;
+    case 33:
+      country = "CAMB"; countryName = "Cambodia";
+      break;
+    case 34:
+      country = "CAME"; countryName = "Cameroon";
+      break;
+    case 35:
+      country = "CANA"; countryName = "Canada";
+      break;
+    case 36:
+      country = "CAVE"; countryName = "Cape Verde";
+      break;
+    case 37:
+      country = "CYIS"; countryName = "Cayman Islands";
+      break;
+    case 38:
+      country = "CAFR"; countryName = "Central African Republic";
+      break;
+    case 39:
+      country = "CHAD"; countryName = "Chad";
+      break;
+    case 40:
+      country = "CHIL"; countryName = "Chile";
+      break;
+    case 41:
+      country = "CHIN"; countryName = "China";
+      break;
+    case 42:
+      country = "CLMB"; countryName = "Colombia";
+      break;
+    case 43:
+      country = "COMO"; countryName = "Comoros";
+      break;
+    case 44:
+      country = "CORC"; countryName = "Costa Rica";
+      break;
+    case 45:
+      country = "CROA"; countryName = "Croatia";
+      break;
+    case 46:
+      country = "CUBA"; countryName = "Cuba";
+      break;
+    case 47:
+      country = "CURA"; countryName = "Curasao";
+      break;
+    case 48:
+      country = "CYPR"; countryName = "Cyprus";
+      break;
+    case 49:
+      country = "CZEC"; countryName = "The Czech Republic";
+      break;
+    case 50:
+      country = "DENM"; countryName = "Denmark";
+      break;
+    case 51:
+      country = "DJIB"; countryName = "Djibouti";
+      break;
+    case 52:
+      country = "DOMN"; countryName = "Dominica";
+      break;
+    case 53:
+      country = "DORE"; countryName = "Dominican Republic";
+      break;
+    case 54:
+      country = "ETIM"; countryName= "East Timor";
+      break;
+    case 55:
+      country = "ECUA"; countryName = "Ecuador";
+      break;
+    case 56:
+      country = "EGYP"; countryName = "Egypt";
+      break;
+    case 57:
+      country = "ELSA"; countryName = "El Salvador";
+      break;
+    case 58:
+      country = "EQGU"; countryName = "Equatorial Guinea";
+      break;
+    case 59:
+      country = "ERIT"; countryName = "Eritrea";
+      break;
+    case 60:
+      country = "ESTN"; countryName = "Estonia";
+      break;
+    case 61:
+      country = "ETHP"; countryName = "Ethiopia";
+      break;
+    case 62:
+      country = "EUUN"; countryName = "European Union";
+      break;
+    case 63:
+      country = "FIJI"; countryName = "Fiji";
+      break;
+    case 64:
+      country = "FINL"; countryName = "Finland";
+      break;
+    case 65:
+      country = "FRAN"; countryName = "France";
+      break;
+    case 66:
+      country = "FPOL"; countryName = "French Polynesia";
+      break;
+    case 67:
+      country = "GAMB"; countryName = "The Gambia";
+      break;
+    case 68:
+      country = "GEOR"; countryName = "Georgia";
+      break;
+    case 69:
+      country = "GERM"; countryName = "Germany";
+      break;
+    case 70:
+      country = "GHAN"; countryName = "Ghana";
+      break;
+    case 71:
+      country = "GIBR"; countryName = "Gibraltar";
+      break;
+    case 72:
+      country = "GREC"; countryName = "Greece";
+      break;
+    case 73:
+      country = "GRLD"; countryName = "Greenland";
+      break;
+    case 74:
+      country = "GREN"; countryName = "Grenada";
+      break;
+    case 75:
+      country = "GUAM"; countryName = "Guam";
+      break;
+    case 76:
+      country = "GUAT"; countryName = "Guatemala";
+      break;
+    case 77:
+      country = "GUER"; countryName = "Guernsey";
+      break;
+    case 78:
+      country = "GUIN"; countryName = "Guinea";
+      break;
+    case 79:
+      country = "GUBA"; countryName = "Guinea-Bissau";
+      break;
+    case 80:
+      country = "GUYA"; countryName = "Guyana";
+      break;
+    case 81:
+      country = "HAIT"; countryName = "Haiti"
+      break;
+    case 82:
+      country = "HOND"; countryName = "Honduras";
+      break;
+    case 83:
+      country = "HUNG"; countryName = "Hungary";
+      break;
+    case 84:
+      country = "ICEL"; countryName = "Iceland";
+      break;
+    case 85:
+      country = "INDN"; countryName = "Indonesia";
+      break;
+    case 86:
+      country = "IRAN"; countryName = "Iran";
+      break;
+    case 87:
+      country = "IRAQ"; countryName = "Iraq";
+      break;
+    case 88:
+      country = "IREL"; countryName = "Ireland";
+      break;
+    case 89:
+      country = "ISMA"; countryName = "Isle of Man";
+      break;
+    case 90:
+      country = "ISRA"; countryName = "Israel";
+      break;
+    case 91:
+      country = "ITAL"; countryName = "Italy";
+      break;
+    case 92:
+      country = "CDIV"; countryName = "Ivory Coast"
+      break;
+    case 93:
+      country = "JAMA"; countryName = "Jamaica";
+      break;
+    case 94:
+      country = "JAPA"; countryName = "Japan";
+      break;
+    case 95:
+      country = "JERS"; countryName = "Jersey";
+      break;
+    case 96:
+      country = "JORD"; countryName = "Jordan";
+      break;
+    case 97:
+      country = "KAZK"; countryName = "Kazakhstan";
+      break;
+    case 98:
+      country = "KENY"; countryName = "Kenya";
+      break;
+    case 99:
+      country = "KIRB"; countryName = "Kiribati";
+      break;
+    case 100:
+      country = "KOSV"; countryName = "Kosovo";
+      break;
+    case 101:
+      country = "KUWA"; countryName = "Kuwait";
+      break;
+    case 102:
+      country = "KYRG"; countryName = "Kyrgyzstan";
+      break;
+    case 103:
+      country = "LAOS"; countryName = "Laos";
+      break;
+    case 104:
+      country = "LATV"; countryName = "Latvia";
+      break;
+    case 105:
+      country = "LEBA"; countryName = "Lebanon";
+      break;
+    case 106:
+      country = "LEST"; countryName = "Lesotho";
+      break;
+    case 107:
+      country = "LIBE"; countryName = "Liberia";
+      break;
+    case 108:
+      country = "LBYA"; countryName = "Libya";
+      break;
+    case 109:
+      country = "LIEC"; countryName = "Liechtenstein";
+      break;
+    case 110:
+      country = "LITH"; countryName = "Lithuania";
+      break;
+    case 111:
+      country = "LUXE"; countryName = "Luxembourg";
+      break;
+    case 112:
+      country = "MACE"; countryName = "Macedonia";
+      break;
+    case 113:
+      country = "MADA"; countryName = "Madagascar";
+      break;
+    case 114:
+      country = "MALW"; countryName = "Malawi";
+      break;
+    case 115:
+      country = "MALS"; countryName = "Malaysia";
+      break;
+    case 116:
+      country = "MALD"; countryName = "Maldives";
+      break;
+    case 117:
+      country = "MALI"; countryName = "Mali";
+      break;
+    case 118:
+      country = "MALT"; countryName = "Malta";
+      break;
+    case 119:
+      country = "MAIS"; countryName = "Marshall Islands";
+      break;
+    case 120:
+      country = "MAUR"; countryName = "Mauritania";
+      break;
+    case 121:
+      country = "MRTS"; countryName = "Mauritius";
+      break;
+    case 122:
+      country = "FRAN"; countryName = "Mayotte";
+      break;
+    case 123:
+      country = "MEXC"; countryName = "Mexico";
+      break;
+    case 124:
+      country = "MICR"; countryName = "Micronesia";
+      break;
+    case 125:
+      country = "MIIS"; countryName = "Midway Islands";
+      break;
+    case 126:
+      country = "MOLD"; countryName = "Moldova";
+      break;
+    case 127:
+      country = "MONA"; countryName = "Monaco";
+      break;
+    case 128:
+      country = "MONG"; countryName = "Mongolia";
+      break;
+    case 129:
+      country = "MNGR"; countryName = "Montenegro";
+      break;
+    case 130:
+      country = "MONT"; countryName = "Montserrat";
+      break;
+    case 131:
+      country = "MORC"; countryName = "Morocco";
+      break;
+    case 132:
+      country = "MOZA"; countryName = "Mozambique";
+      break;
+    case 133:
+      country = "MYAN"; countryName = "Myanmar";
+      break;
+    case 134:
+      country = "NAMB"; countryName = "Namibia";
+      break;
+    case 135:
+      country = "NAUR"; countryName = "Nauru";
+      break;
+    case 136:
+      country = "NEPA"; countryName = "Nepal";
+      break;
+    case 137:
+      country = "NETH"; countryName = "The Netherlands";
+      break;
+    case 138:
+      country = "NWZE"; countryName = "New Zealand";
+      break;
+    case 139:
+      country = "NICA"; countryName = "Nicaragua";
+      break;
+    case 140:
+      country = "NIGR"; countryName = "Niger";
+      break;
+    case 141:
+      country = "NGRA"; countryName = "Nigeria";
+      break;
+    case 142:
+      country = "NIUE"; countryName = "Niue";
+      break;
+    case 143:
+      country = "NKOR"; countryName = "North Korea";
+      break;
+    case 144:
+      country = "NORW"; countryName = "Norway";
+      break;
+    case 145:
+      country = "OMAN"; countryName = "Oman";
+      break;
+    case 146:
+      country = "PAKS"; countryName = "Pakistan";
+      break;
+    case 147:
+      country = "PALA"; countryName = "Palau";
+      break;
+    case 148:
+      country = "WEBA"; countryName = "Palestine";
+      break;
+    case 149:
+      country = "PANA"; countryName = "Panama";
+      break;
+    case 150:
+      country = "PANG"; countryName = "Papua New Guinea";
+      break;
+    case 151:
+      country = "PERU"; countryName = "Peru";
+      break;
+    case 152:
+      country = "PHIL"; countryName = "Philippines";
+      break;
+    case 153:
+      country = "PIIS"; countryName = "Pitcairn Islands";
+      break;
+    case 154:
+      country = "POLA"; countryName = "Poland";
+      break;
+    case 155:
+      country = "PORT"; countryName = "Portugal";
+      break;
+    case 156:
+      country = "QATA"; countryName = "Qatar";
+      break;
+    case 157:
+      country = "RMNA"; countryName = "Romania";
+      break;
+    case 158:
+      country = "RUSS"; countryName = "Russian Federation";
+      break;
+    case 159:
+      country = "RWAN"; countryName = "Rwanda";
+      break;
+    case 160:
+      country = "STKN"; countryName = "St. Kitts & Nevis";
+      break;
+    case 161:
+      country = "STLU"; countryName = "Saint Lucia";
+      break;
+    case 162:
+      country = "STPM"; countryName = "St. Pierre & Miquelon";
+      break;
+    case 163:
+      country = "SVGR"; countryName = "St. Vincent & the Grenadines";
+      break;
+    case 164:
+      country = "SAMI"; countryName = "Sami";
+      break;
+    case 165:
+      country = "SAMO"; countryName = "Samoa";
+      break;
+    case 166:
+      country = "SAMA"; countryName = "San Marino";
+      break;
+    case 167:
+      country = "SAAR"; countryName = "Saudi Arabia";
+      break;
+    case 168:
+      country = "SEBO"; countryName = "Seborga";
+      break;
+    case 169:
+      country = "SENE"; countryName = "Senegal";
+      break;
+    case 170:
+      country = "SERB"; countryName = "Serbia";
+      break;
+    case 171:
+      country = "SEYC"; countryName = "Seychelles";
+      break;
+    case 172:
+      country = "SILE"; countryName = "Sierra Leone";
+      break;
+    case 173:
+      country = "SING"; countryName = "Singapore";
+      break;
+    case 174:
+      country = "SIMA"; countryName = "Sint Maarten";
+      break;
+    case 175:
+      country = "SVKA"; countryName = "Slovakia";
+      break;
+    case 176:
+      country = "SLVA"; countryName = "Slovenia";
+      break;
+    case 177:
+      country = "SOIS"; countryName = "Solomon Islands";
+      break;
+    case 178:
+      country = "SMLA"; countryName = "Somalia";
+      break;
+    case 179:
+      country = "SMLD"; countryName = "Somaliland";
+      break;
+    case 180:
+      country = "SOAF"; countryName = "South Africa";
+      break;
+    case 181:
+      country = "SKOR"; countryName = "South Korea";
+      break;
+    case 182:
+      country = "SOSU"; countryName = "South Sudan";
+      break;
+    case 183:
+      country = "SPAN"; countryName = "Spain";
+      break;
+    case 184:
+      country = "SRLA"; countryName = "Sri Lanka";
+      break;
+    case 185:
+      country = "SUDA"; countryName = "The Sudan";
+      break;
+    case 186:
+      country = "SURN"; countryName = "Suriname";
+      break;
+    case 187:
+      country = "SZLD"; countryName = "Swaziland";
+      break;
+    case 188:
+      country = "SWDN"; countryName = "Sweden";
+      break;
+    case 189:
+      country = "SWIT"; countryName = "Switzerland";
+      break;
+    case 190:
+      country = "SYRA"; countryName = "Syria";
+      break;
+    case 191:
+      country = "TAIW"; countryName = "Taiwan";
+      break;
+    case 192:
+      country = "TAJK"; countryName = "Tajikistan";
+      break;
+    case 193:
+      country = "TANZ"; countryName = "Tanzania";
+      break;
+    case 194:
+      country = "THAL"; countryName = "Thailand";
+      break;
+    case 195:
+      country = "TIBE"; countryName = "Tibet";
+      break;
+    case 196:
+      country = "ETIM"; countryName = "Timor-Leste";
+      break;
+    case 197:
+      country = "TOGO"; countryName = "Togo";
+      break;
+    case 198:
+      country = "TOKE"; countryName = "Tokelau";
+      break;
+    case 199:
+      country = "TONG"; countryName = "Tonga";
+      break;
+    case 200:
+      country = "TRTB"; countryName = "Trinidad & Tobago";
+      break;
+    case 201:
+      country = "TRDC"; countryName = "Tristan da Cunha";
+      break;
+    case 202:
+      country = "TUNS"; countryName = "Tunisia";
+      break;
+    case 203:
+      country = "TURK"; countryName = "Turkey";
+      break;
+    case 204:
+      country = "TKST"; countryName = "Turkmenistan";
+      break;
+    case 205:
+      country = "TUCS"; countryName = "Turks & Caicos Islands";
+      break;
+    case 206:
+      country = "TUVA"; countryName = "Tuvalu";
+      break;
+    case 207:
+      country = "UGAN"; countryName = "Uganda";
+      break;
+    case 208:
+      country = "UKRN"; countryName = "Ukraine";
+      break;
+    case 209:
+      country = "UAEM"; countryName = "United Arab Emirates";
+      break;
+    case 210:
+      country = "UNKG"; countryName = "United Kingdom";
+      break;
+    case 211:
+      country = "UNNA"; countryName = "United Nations";
+      break;
+    case 212:
+      country = "UNST"; countryName = "United States";
+      break;
+    case 213:
+      country = "URGY"; countryName = "Uruguay";
+      break;
+    case 214:
+      country = "UZBK"; countryName = "Uzbekistan";
+      break;
+    case 215:
+      country = "VANT"; countryName = "Vanuatu";
+      break;
+    case 216:
+      country = "VACY"; countryName = "Vatican City";
+      break;
+    case 217:
+      country = "VENZ"; countryName = "Venezuela";
+      break;
+    case 218:
+      country = "VIET"; countryName = "Vietnam";
+      break;
+    case 219:
+      country = "USVS"; countryName = "Virgin Islands";
+      break;
+    case 220:
+      country = "WAIS"; countryName = "Wake Island";
+      break;
+    case 221:
+      country = "YEMN"; countryName = "Yemen";
+      break;
+    case 222:
+      country = "ZAMB"; countryName = "Zambia";
+      break;
+    case 223:
+      country = "ZBWE"; countryName = "Zimbabwe";
+      break;
+    default:
+      country = "UNST"; countryName = "United States";
+  }
+  document.getElementById('FlagImage').src='http://www.flags.net/images/largeflags/'+country+'0001.GIF'
+  document.getElementById('Title').innerHTML=countryName
+}
